9acab8425b897185085dd4ffd487c56ffb6ce01c
Marco Maggini <m.maggini@adiacent.com>
2026-01-13 10:35:16 +0100

    Init DB and products on startup

--- utility.py
+++ utility.py
@@ -11,9 +11,9 @@ def init_products():
     if db.query(ProductDB).count() == 0:
 
         products = [
-            ProductDB(code="P001", description="Prodotto 1", imageUrl="img1.png", price=10.0),
-            ProductDB(code="P002", description="Prodotto 2", imageUrl="img2.png", price=15.5),
-            ProductDB(code="P003", description="Prodotto 3", imageUrl="img3.png", price=7.25)
+            ProductDB(code="P001", description="Prodotto 1", imageUrl="https://picsum.photos/300/300?random=1", price=10.0),
+            ProductDB(code="P002", description="Prodotto 2", imageUrl="https://picsum.photos/300/300?random=2", price=15.5),
+            ProductDB(code="P003", description="Prodotto 3", imageUrl="https://picsum.photos/300/300?random=3", price=7.25)
         ]
         db.add_all(products)
         db.commit()
